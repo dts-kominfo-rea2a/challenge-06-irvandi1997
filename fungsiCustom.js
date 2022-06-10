5357b0f882a17c3a07bd1bb9cd97ce56722bdca8
irvandi1997 <106160897+irvandi1997@users.noreply.github.com>
2022-06-10 21:22:34 +0700

    tantangan js callback part 2

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require('fs');
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +19,28 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fnCallback) => {
+  let hasilData = [];
+  fs.readFile(file1, { encoding: 'utf-8' }, (err, data) => {
+    if (err) {
+      return fnCallback(err, null);
+    }
+    hasilData.push(JSON.parse(data).message.split(" ")[1])
+    fs.readFile(file2, { encoding: 'utf-8' }, (err, data) => {
+      if (err) {
+        return fnCallback(err, null);
+      }
+      hasilData.push(JSON.parse(data)[0].message.split(" ")[1])
+      fs.readFile(file3, { encoding: 'utf-8' }, (err, data) => {
+        if (err) {
+          return fnCallback(err, null);
+        }
+        hasilData.push(JSON.parse(data)[0].data.message.split(" ")[1])
+        fnCallback(null, hasilData);
+      })
+    })
+  });
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
